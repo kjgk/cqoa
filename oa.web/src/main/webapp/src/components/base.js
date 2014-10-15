446b92630daedc05282fbfdb2155afec936e65b0
kjgk <kjgk123@126.com>
2014-10-15 10:09:09 +0800

    update

--- oa.web/src/main/webapp/src/components/base.js
+++ oa.web/src/main/webapp/src/components/base.js
@@ -2,7 +2,7 @@
 
 angular.module('base', ['restangular', 'ui.bootstrap', 'ui.router'
     , 'validation', 'validation.rule', 'angular-loading-bar', 'toaster'
-    , 'ngQuickDate','mgcrea.ngStrap.datepicker','mgcrea.ngStrap.timepicker'
+    , 'mgcrea.ngStrap.datepicker', 'mgcrea.ngStrap.timepicker'
 
     , 'withub.ui.table', 'withub.filter'
 ])
