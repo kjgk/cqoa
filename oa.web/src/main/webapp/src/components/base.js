394b387874d98d719b8c408c51c8d5e2581421a4
kjgk <kjgk123@126.com>
2014-11-06 12:42:23 +0800

    update

--- oa.web/src/main/webapp/src/components/base.js
+++ oa.web/src/main/webapp/src/components/base.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('base', [
-    'ngSanitize', 'ngAnimate', 'restangular', 'ui.bootstrap', 'ui.router'
+    'ngSanitize', 'ngAnimate', 'restangular'
+    ,'ui.router', 'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ui.bootstrap.datetimepicker', 'ui.dateTimeInput'
     , 'validation', 'validation.rule', 'angular-loading-bar', 'toaster', 'cgBusy'
     , 'mgcrea.ngStrap.datepicker', 'mgcrea.ngStrap.timepicker', 'uuid4', 'ui.select', 'angularFileUpload', 'monospaced.qrcode'
     , 'withub.common', 'withub.ui', 'withub.filter'
