b2bb56b32d4176d6c928297c830044b88e301ca3
kjgk <kjgk123@126.com>
2014-11-13 17:28:43 +0800

    `

--- oa.web/src/main/webapp/gulp/build.js
+++ oa.web/src/main/webapp/gulp/build.js
@@ -61,7 +61,7 @@ gulp.task('html', ['styles', 'scripts', 'partials'], function () {
         .pipe($.uglify({preserveComments: $.uglifySaveLicense}))
         .pipe(jsFilter.restore())
         .pipe(cssFilter)
-        .pipe($.replace('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', 'fonts'))
+        .pipe($.replace('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', 'dist/fonts'))
         .pipe($.csso())
         .pipe(cssFilter.restore())
         .pipe(assets.restore())
