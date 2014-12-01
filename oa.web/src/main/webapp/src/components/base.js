af3d4d0fdde38a3f58fe2859bf7bfba58b045a62
kjgk <kjgk123@126.com>
2014-12-01 16:53:02 +0800

    1

--- oa.web/src/main/webapp/src/components/base.js
+++ oa.web/src/main/webapp/src/components/base.js
@@ -4,7 +4,7 @@ angular.module('base', [
     'ngSanitize', 'ngAnimate', 'restangular'
     ,'ui.router', 'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ui.bootstrap.datetimepicker', 'ui.bootstrap.pagination', 'ui.dateTimeInput'
     , 'validation', 'validation.rule', 'angular-loading-bar', 'toaster', 'cgBusy', 'ngDialog'
-    , 'uuid4', 'ui.select', 'angularFileUpload', 'monospaced.qrcode', 'ui.tree'
+    , 'uuid4', 'angularFileUpload', 'monospaced.qrcode', 'ui.tree'
     , 'withub.common', 'withub.ui', 'withub.filter'
 ])
 ;
